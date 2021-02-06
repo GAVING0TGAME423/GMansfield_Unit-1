3333a1eb51e3ec76a04d53c7a3ad92686148b60e
GAVING0TGAME423 <gavincole423@gmail.com>
2021-02-05 16:56:40 -0800

    Car hits 3 barricades
    title

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Debug.Log(Time.deltaTime);
+        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+    }
+}
